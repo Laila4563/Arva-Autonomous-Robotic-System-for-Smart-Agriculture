9826066eab321766c828a47bb04d621f993ec53d
Alaa Waleed <aw827948@gmail.com>
2026-01-31 12:04:04 +0200

    Final Pump Code

--- C++/Actuators/Pump.cpp
+++ C++/Actuators/Pump.cpp
@@ -2,52 +2,39 @@
 
 void Pump::claimPins() {
     if (powerPin_ >= 0) assignPin(powerPin_);
-    if (flowRatePin_ >= 0) assignPin(flowRatePin_);
 
     if (powerPin_ >= 0) pinMode(powerPin_, OUTPUT);
 
-    if (flowRatePin_ >= 0) {
-        pinMode(flowRatePin_, PWM_OUTPUT);
-        pwmSetMode(PWM_MODE_MS);
-        pwmSetRange(pumpScaler);
-    }
 
     if (powerPin_ >= 0) digitalWrite(powerPin_, LOW);
-    if (flowRatePin_ >= 0) pwmWrite(flowRatePin_, 0);
+
 }
 
 void Pump::releasePins() {
     if (powerPin_ >= 0) deassignPin(powerPin_);
-    if (flowRatePin_ >= 0) deassignPin(flowRatePin_);
 }
 
-Pump::Pump(int powerPin, int flowRatePin)
-    : powerPin_(powerPin), flowRatePin_(flowRatePin), on_(false)
+Pump::Pump(int powerPin)
+    : powerPin_(powerPin), on_(false)
 {
     claimPins();
-    setFlowRate(100.f); 
+
 }
 
 Pump::Pump(const Pump& other) noexcept
-    : powerPin_(other.powerPin_), flowRatePin_(other.flowRatePin_),
-    flowRate_(other.flowRate_), on_(false)
+    : powerPin_(other.powerPin_), on_(false)
 {
     claimPins();
-    pwmWrite(flowRatePin_, flowRate_);
     digitalWrite(powerPin_, LOW);
 }
 
 Pump::Pump(Pump&& other) noexcept
-    : powerPin_(other.powerPin_), flowRatePin_(other.flowRatePin_),
-    flowRate_(other.flowRate_), on_(other.on_)
+    : powerPin_(other.powerPin_), on_(other.on_)
 {
     other.powerPin_ = -1;
-    other.flowRatePin_ = -1;
-    other.flowRate_ = 0;
     other.on_ = false;
 
     claimPins();
-    pwmWrite(flowRatePin_, flowRate_);
     digitalWrite(powerPin_, LOW);
 }
 
@@ -56,12 +43,9 @@ Pump& Pump::operator=(const Pump& other) noexcept {
         releasePins();
 
         powerPin_ = other.powerPin_;
-        flowRatePin_ = other.flowRatePin_;
-        flowRate_ = other.flowRate_;
         on_ = false;
 
         claimPins();
-        pwmWrite(flowRatePin_, flowRate_);
         digitalWrite(powerPin_, LOW);
     }
     return *this;
@@ -72,17 +56,12 @@ Pump& Pump::operator=(Pump&& other) noexcept {
         releasePins();
 
         powerPin_ = other.powerPin_;
-        flowRatePin_ = other.flowRatePin_;
-        flowRate_ = other.flowRate_;
         on_ = other.on_;
 
         other.powerPin_ = -1;
-        other.flowRatePin_ = -1;
-        other.flowRate_ = 0;
         other.on_ = false;
 
         claimPins();
-        pwmWrite(flowRatePin_, flowRate_);
         digitalWrite(powerPin_, LOW);
     }
     return *this;
@@ -102,14 +81,9 @@ bool Pump::isOn() const {
     return on_;
 }
 
-void Pump::setFlowRate(float percentage) {
-    int pwm = percentageToPwm(percentage);
-    flowRate_ = pwm;
-    if (flowRatePin_ >= 0) pwmWrite(flowRatePin_, pwm);
-}
+
 
 Pump::~Pump() {
     turnOff();
-    if (flowRatePin_ >= 0) pwmWrite(flowRatePin_, 0);
     releasePins();
 }
--- C++/Actuators/Pump.h
+++ C++/Actuators/Pump.h
@@ -1,13 +1,11 @@
 #pragma once
-#include <wiringPi.h>
+#include "../GPIO/gpio.h"
 #include <atomic>
 #include "../Config/Config.h"
 
-#define pumpScaler 1024
-
 class Pump {
 public:
-    Pump(int powerPin, int flowRatePin);
+    Pump(int powerPin);
     Pump(const Pump& other) noexcept;
     Pump(Pump&& other) noexcept;
     Pump& operator=(const Pump& other) noexcept;
@@ -18,20 +16,11 @@ public:
     void turnOff();
     bool isOn() const;
 
-    void setFlowRate(float percentage); 
-
 private:
     int powerPin_{ -1 };
-    int flowRatePin_{ -1 };
-    int flowRate_{ 0 };
     bool on_{ false };
 
     void claimPins();
     void releasePins();
 
-    static inline int percentageToPwm(float percentage) {
-        if (percentage < 0.f) percentage = 0.f;
-        if (percentage > 100.f) percentage = 100.f;
-        return static_cast<int>((percentage * pumpScaler) / 100.f);
-    }
 };
